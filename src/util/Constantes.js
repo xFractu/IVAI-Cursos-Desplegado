eb4efbc819a61a7d2478f84e3de273e4791d534d
xFractu <marioteku99@gmail.com>
2025-02-17 11:13:04 -0600

    Prueba

--- src/util/Constantes.js
+++ src/util/Constantes.js
@@ -1,2 +1,3 @@
-// export const API_URL = 'http://localhost:4567/';
-export const API_URL = 'http://187.216.225.247:4567/';
+export const API_URL = 'http://localhost:4567/';
+//export const API_URL = 'http://187.216.225.247:4567/';
+
