73a9c358ad55b8c0fe5849cfe8387a6203b55793
AngelSuarez03 <Angnusa11@gmail.com>
2025-02-19 12:57:50 -0600

    Constante URL despleago

--- src/util/Constantes.js
+++ src/util/Constantes.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:4567/';
-// export const API_URL = 'http://187.216.225.247:4567/';
+// export const API_URL = 'http://localhost:4567/';
+export const API_URL = 'http://187.216.225.247:4567/';
